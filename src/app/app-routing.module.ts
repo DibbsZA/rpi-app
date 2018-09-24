34b6babbdc17e4135df45e8fb41fd1e5371ec2ab
Gary de Beer <garyd@Garys-MacBook-Pro.local>
2018-09-25 00:32:52 +0200

    made About the default home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './core/auth.guard';
 
 const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: 'about', pathMatch: 'full' },
     { path: 'home', loadChildren: './ui/home/home.module#HomePageModule', canLoad: [] },
-    // { path: 'list', loadChildren: './ui/list/list.module#ListPageModule', canActivate: [AuthGuard] },
+    { path: 'registration', loadChildren: './ui/registration/registration.module#RegistrationPageModule' },
     { path: 'scan', loadChildren: './ui/scan/scan.module#ScanPageModule', canActivate: [AuthGuard] },
     { path: 'pay', loadChildren: './ui/pay/pay.module#PayPageModule', canActivate: [AuthGuard] },
     { path: 'about', loadChildren: './ui/about/about.module#AboutPageModule' },
     { path: 'profile', loadChildren: './ui/profile/profile.module#ProfilePageModule', canActivate: [AuthGuard] },
-    { path: 'registration', loadChildren: './ui/registration/registration.module#RegistrationPageModule' },
-    { path: 'account', loadChildren: './ui/account/account.module#AccountPageModule' },
-  { path: 'history', loadChildren: './ui/history/history.module#HistoryPageModule' },
-  { path: 'pin-confirm', loadChildren: './ui/pin-confirm/pin-confirm.module#PinConfirmPageModule' }
+    { path: 'account', loadChildren: './ui/account/account.module#AccountPageModule', canActivate: [AuthGuard] },
+    { path: 'history', loadChildren: './ui/history/history.module#HistoryPageModule', canActivate: [AuthGuard] },
+    { path: 'pin-confirm', loadChildren: './ui/pin-confirm/pin-confirm.module#PinConfirmPageModule', canActivate: [AuthGuard] },
+    { path: 'txn-detail/:id', loadChildren: './ui/txn-detail/txn-detail.module#TxnDetailPageModule', canActivate: [AuthGuard] }
 ];
 
 @NgModule({
